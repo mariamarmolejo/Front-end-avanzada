3f75eacc70c4b402775ce59db10407cd6d9b3fa7
Maria Jose M <ingmariamarmolejo@gmail.com>
2025-05-14 22:49:46 -0500

    se divide pantalla de mapa y se agrega vista reporte

--- src/app/core/app.routes.server.ts
+++ src/app/core/app.routes.server.ts
@@ -1,6 +1,17 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import {ReportService} from "./services/report.service";
+import {inject} from "@angular/core";
+import {map} from "rxjs/operators";
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'report/edit/:id',
+    renderMode: RenderMode.Server // or remove the renderMode property
+  },
+  {
+    path: 'categories/edit/:id',
+    renderMode: RenderMode.Server // or remove the renderMode property
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
